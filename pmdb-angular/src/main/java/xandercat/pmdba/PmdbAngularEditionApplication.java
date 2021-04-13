4c268b8a88891e8c57b22c8cb93cbeb138b9d2d9
Scott Arnold <scottcarnold@gmail.com>
2021-04-13 18:42:49 -0500

    Initial commit

---
+++ pmdb-angular/src/main/java/xandercat/pmdba/PmdbAngularEditionApplication.java
@@ -0,0 +1,13 @@
+package xandercat.pmdba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdbAngularEditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdbAngularEditionApplication.class, args);
+	}
+
+}
---
+++ pmdb-angular/src/test/java/xandercat/pmdba/PmdbAngularEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package xandercat.pmdba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmdbAngularEditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
