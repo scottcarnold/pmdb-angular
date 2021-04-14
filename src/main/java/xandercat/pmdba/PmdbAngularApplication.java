65881052c8869cc15af0346bf6ed34b20877c991
Scott Arnold <scottcarnold@gmail.com>
2021-04-13 19:21:52 -0500

    Rename application class

--- src/main/java/xandercat/pmdba/PmdbAngularApplication.java
+++ src/main/java/xandercat/pmdba/PmdbAngularApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PmdbAngularEditionApplication {
+public class PmdbAngularApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PmdbAngularEditionApplication.class, args);
+		SpringApplication.run(PmdbAngularApplication.class, args);
 	}
 
 }
