118da4752cafc36b26a801e4c646120a0a9e3cc3
Scott Arnold <scottcarnold@gmail.com>
2021-12-29 15:21:19 -0600

    Verify access key functionality for code commit.

--- src/main/java/org/xandercat/pmdba/PmdbAngularApplication.java
+++ src/main/java/org/xandercat/pmdba/PmdbAngularApplication.java
@@ -3,6 +3,7 @@ package org.xandercat.pmdba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// benign update to verify functionality of access token in Eclipse
 @SpringBootApplication
 public class PmdbAngularApplication {
 
